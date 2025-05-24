83fcc45ce0f5046bf59bc9a01688cb7fb0be6028
Banti Patel <bantipatel2506@gmail.com>
2025-05-24 13:19:30 +0530

    fix: import in applayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,8 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 
+import Header from './Header';
 import Sidebar from './Sidebar';
-import Header from './header';
 
 const StyleAppLayout = styled.div`
   display: grid;
